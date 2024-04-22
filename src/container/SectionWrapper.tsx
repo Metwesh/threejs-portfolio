6a659c0b06800a143d5467a25094901e9448dee5
Mohamed Hani <mohamed.hani@sideup.co>
2024-04-22 02:40:11 +0200

    Reduced viewport threshold to fix cards not showing

--- src/container/SectionWrapper.tsx
+++ src/container/SectionWrapper.tsx
@@ -19,7 +19,7 @@ export default function SectionWrapper(props: {
       )}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: isMobile ? 0.1 : 0.25 }}
+      viewport={{ once: true, amount: isMobile ? 0.075 : 0.25 }}
       className={`${props.className ?? ""} ${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
       <span className="hash-span" id={props.id}>
